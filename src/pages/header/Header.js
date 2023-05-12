7b94681078ef74d561ad636302d143c09db18201
javier-tralma <javier.tralma@sansano.usm.cl>
2023-05-12 01:29:56 -0400

    feat: logout implemented

--- src/pages/header/Header.js
+++ src/pages/header/Header.js
@@ -1,13 +1,23 @@
-import { Inter } from 'next/font/google';
-import Image from 'next/image';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import styles from './Header.module.css';
-import perfil from './perfil.png';
+/* eslint-disable no-unused-vars */
+import { Inter } from 'next/font/google'
+import Image from 'next/image'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import styles from './Header.module.css'
+import perfil from './perfil.png'
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] })
 
-export default function Header() {
+export default function Header () {
+  const handleLogout = () => {
+    // Elimina la información del usuario del localStorage
+    localStorage.removeItem('user')
+    localStorage.removeItem('token')
+
+    // Redirige al usuario a la ruta raíz
+    window.location.href = '/'
+  }
+
   return (
     <Navbar className={styles['navbar-custom']}>
       <Container>
@@ -18,10 +28,10 @@ export default function Header() {
             <Nav.Link className={styles['nav-link']}>
               <Image src={perfil} width={32} height={32} alt="Perfil" />
             </Nav.Link>
-            <Nav.Link className={styles['nav-link']} href="#contact">Cerrar Sesión</Nav.Link>
+            <Nav.Link className={styles['nav-link']} onClick={handleLogout}>Cerrar Sesión</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  );
+  )
 }
