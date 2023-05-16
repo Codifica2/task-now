7eb64525ee5b50fca26ca7ba3112cda07b371bb3
javier-tralma <javier.tralma@sansano.usm.cl>
2023-05-15 21:55:23 -0400

    feat: jwt token verifier added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,30 @@
 /* eslint-disable no-unused-vars */
 import { TaskProvider } from '@/context/taskContext.js'
 import withAuth from '../../withAuth'
+import { useUserContext } from '@/context/auth-context.js'
 
 import Header from './header/Header'
 import Dashboard from './dashboard/Dashboard'
+import React, { useEffect, useState } from 'react'
+// eslint-disable-next-line camelcase
+import jwt_decode from 'jwt-decode'
 
 function Home () {
+  const { setUser, setToken } = useUserContext()
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      const decodedToken = jwt_decode(token)
+      const currentTime = Date.now() / 1000
+      if (decodedToken.exp < currentTime) {
+        setUser(null)
+        setToken(null)
+        window.location.reload()
+      }
+    } else {
+      window.location.reload()
+    }
+  }, [])
   return (
     <>
       <TaskProvider>
